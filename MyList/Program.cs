f4679e0dccb3552b7a76b4be5f2282d3dca14b6f
Vadka.casper@gmail.com <Vadka.casper@gmail.com>
2020-08-26 20:48:39 +0700

    Added an indexer for accessing a list item

--- MyList/Program.cs
+++ MyList/Program.cs
@@ -21,6 +21,82 @@ namespace MyList
         private int Size;
 
         /// <summary>
+        /// Поиск обьекта по данным
+        /// </summary>
+        /// <param name="Wanted">Объект</param>
+        /// <param name="data">Данные для поиска</param>
+        /// <returns>Искомый объекет или null</returns>
+        private NewList<T> Search (NewList<T> Wanted, T data)
+        {
+            while (Wanted != null)
+            {
+                if (Wanted.Data.Equals(data))
+                {
+                    break;
+                }
+                Wanted = Wanted.Next;
+            }
+            return Wanted;
+        }
+
+        /// <summary>
+        /// Поиск объекта по индексу
+        /// </summary>
+        /// <param name="Wanted">Объект</param>
+        /// <param name="index">Индекс</param>
+        /// <returns>Искомый объект или null</returns>
+        private NewList<T> Search(NewList<T> Wanted, int index)
+        {
+            int item = 0;
+            if (index < Size)
+            {
+                while (Wanted != null)
+                {
+                    if (item == index)
+                    {
+                        break;
+                    }
+                    item++;
+                    Wanted = Wanted.Next;
+                }
+                return Wanted;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Обращение по индексу к объекту списка
+        /// </summary>
+        /// <param name="index">Индекс объекта</param>
+        /// <returns>Данные Объекта</returns>
+        public T this[int index] 
+        {
+            get
+            {
+                NewList<T> temp = Head;
+                temp = Search(temp, index);
+                if (temp != null)
+                 {
+                    return temp.Data;
+                 }
+                 else
+                     throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                NewList<T> temp = Head;
+                temp = Search(temp, index);
+                if (temp != null)
+                {
+                    temp.Data = value;
+                }
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+
+        /// <summary>
         /// Добовляет объет в конец списка
         /// </summary>
         /// <param name="data">Объект</param>
@@ -72,14 +148,7 @@ namespace MyList
         {
             NewList<T> temp = Head;
 
-            while (temp != null)
-            {
-                if (temp.Data.Equals(data))
-                {
-                    break;
-                }
-                temp = temp.Next;
-            }
+            temp = Search(temp, data);
 
             if (temp != null)
             {
@@ -112,21 +181,10 @@ namespace MyList
         public bool RemoveAt (int index)
         {
             NewList<T> temp = Head;
-            int item = 0;
-
             if (index < Size && index >= 0)
             {
-
-                while (temp != null)
-                {
-                    if (item == index)
-                    {
-                        break;
-                    }
-                    item++;
-                    temp = temp.Next;
-                }
-
+                temp = Search(temp, index);
+                
                 if (temp.Prev != null && temp.Next != null)
                 {
                     temp.Prev.Next = temp.Next;
@@ -188,7 +246,7 @@ namespace MyList
         static void Main(string[] args)
         {
             DoubleLinkList<int> X = new DoubleLinkList<int>();
-
+            
 
             for(int i = 0; i < 15; i++)
             {
@@ -196,11 +254,10 @@ namespace MyList
             }
             X.Print();
             Console.Write("\n");
-
             X.Remove(5);
-
+            X[11] = 10;
             X.Print();
-            Console.Write($"\nSize = {X.SizeList()}");
+            Console.Write($"\nSize = {X.SizeList()} {X[11]}");
             Console.Read();
         }
     }
